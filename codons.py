65a5411b624a4c467d2a4034e723aaf26b35c204
Dor-sketch <dor.pascal@mail.huji.ac.il>
2024-12-08 13:09:26 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -1,4 +1,8 @@
-def create_codon_dict(file_path):
-    pass # Replace the pass with your code
-
+def create_codon_dict(filename):
+    codon_dict = {}
+    with open(filename, 'r') as file:
+        for line in file:
+            codon, amino_acid, single_letter, full_name = line.split()
+            codon_dict[codon] = single_letter
+    return codon_dict
 
